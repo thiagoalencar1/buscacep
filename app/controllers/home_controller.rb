29209ee488a76347d131c6b72f1de2d7e7f5b5df
Thiago Alencar <talencar@gmail.com>
2025-02-04 15:52:52 -0300

    feat: Get and show CEP informations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+    @cep = params[:find_cep]
+  end
+
+  def find_cep
+    @cep = params[:find_cep]
+    response = Faraday.get("https://cep.awesomeapi.com.br/json/#{@cep}")
+    @address = JSON.parse(response.body).symbolize_keys
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  post "home/index" => "home#find_cep"
 end
