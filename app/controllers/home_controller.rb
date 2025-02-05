3b67f1b52c90708903e1a3db96793bebadbb31d3
Thiago Alencar <talencar@gmail.com>
2025-02-05 09:42:12 -0300

    feat: Adjustments in view, refactor controller and tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,28 +1,25 @@
 class HomeController < ApplicationController
   def index
-    @cep = params[:find_cep]
   end
 
   def find_cep
     @cep = params[:find_cep]
-    return unless validate_cep(@cep)
     @address = get_address(@cep)
+
+    if get_address(@cep)[:code]
+      @error = get_address(@cep)[:message]
+    else
+      @address = get_address(@cep)
+    end
+
     render :index
   end
 
   private
 
-  def validate_cep(cep)
-    cep.gsub(/[\.\-]/, '').match?(/\A\d{8}\z/)
-  end
-
   def get_address(cep)
-    response = Faraday.get("https://cep.awesomeapi.com.br/json/#{cep}")
+    response ||= Faraday.get("https://cep.awesomeapi.com.br/json/#{cep}")
 
     JSON.parse(response.body).symbolize_keys
-  end
-
-  def cep_params
-    params.require(:cep).permit(:find_cep)
   end
 end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -10,20 +10,46 @@ RSpec.describe "Home", type: :request do
   end
 
   describe "#find_cep" do
+    before(:each) do
+      allow_any_instance_of(ActionController::Base).to receive(:protect_against_forgery?).and_return(false)
+    end
+
     it "should find cep" do
       cep = '05424020'
   
-      stub_request(:any, "https://cep.awesomeapi.com.br/json/#{cep}")
+      stub_request(:get, "https://cep.awesomeapi.com.br/json/#{cep}")
         .to_return(body: { address: 'Rua Professor Carlos Reis', city: 'São Paulo', state: 'SP' }.to_json)
 
-      allow_any_instance_of(ActionController::Base).to receive(:protect_against_forgery?).and_return(false)
-
-      post find_cep_path, params: { find_cep: "05424020"}
+      post find_cep_path, params: { find_cep: cep }
 
       expect(response).to have_http_status(:success)
       expect(response.body).to include("Rua Professor Carlos Reis")
       expect(response.body).to include("São Paulo")
       expect(response.body).to include("SP")
+    end
+
+    it "if cep is invalid" do
+      cep = '12.234-99'
+  
+      stub_request(:get, "https://cep.awesomeapi.com.br/json/#{cep}")
+        .to_return(body: {"code":"invalid","message":"CEP invalido, tente: 00000000"}.to_json)
+
+      post find_cep_path, params: { find_cep: cep }
+  
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include("CEP invalido")
+    end
+  
+    it "if cep contain invalid characters" do
+      cep = '4342adf'
+  
+      stub_request(:get, "https://cep.awesomeapi.com.br/json/#{cep}")
+        .to_return(body: {"code":"invalid","message":"CEP invalido, tente: 00000000"}.to_json)
+
+      post find_cep_path, params: { find_cep: cep }
+  
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include("CEP invalido")
     end
   end
 end
